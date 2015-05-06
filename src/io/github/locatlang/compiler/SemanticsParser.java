c412b8d97d033aff2fdfe26cc302b9c528ef9573
Creeps <creeper32605@gmail.com>
2015-05-06 19:31:54 +0200

    Revert "make switches if statements"
    This reverts commit f3b14d0f8236ebe18ca1b0776103855fd319096d.

--- src/io/github/locatlang/compiler/SemanticsParser.java
+++ src/io/github/locatlang/compiler/SemanticsParser.java
@@ -57,18 +57,23 @@ public class SemanticsParser {
         matcher = makeMatcher("^\\s*(public|private|protected)\\s+(static|main|)\\s*class\\s+(\\w+)\\s*(?:extends\\s*(\\w+)\\s*|)(?:implements\\s*((?:\\w+)(?:\\s*,\\s*\\w+)*)\\s*|)\\{$", instruction);
         if(matcher.matches()) {
             ClassProtection protection = ClassProtection.PUBLIC;
-            if(matcher.group(1).equals("public")) {
-                protection = ClassProtection.PUBLIC;
-            } else if(matcher.group(1).equals("private")) {
-                protection = ClassProtection.PRIVATE;
-            } else if(matcher.group(1).equals("protected")) {
+            switch(matcher.group(1)) {
+                case "public":
+                    protection = ClassProtection.PUBLIC;
+                    break;
+                case "private":
+                    protection = ClassProtection.PRIVATE;
+                    break;
+                case "protected":
                     protection = ClassProtection.PROTECTED;
             }
 
             ClassScope scope = ClassScope.DEFAULT;
-            if(matcher.group(2).equals("static")) {
-                scope = ClassScope.STATIC;
-            } else if(matcher.group(2).equals("main")) {
+            switch(matcher.group(2)) {
+                case "static":
+                    scope = ClassScope.STATIC;
+                    break;
+                case "main":
                     scope = ClassScope.MAIN;
             }
 
@@ -89,12 +94,15 @@ public class SemanticsParser {
         matcher = makeMatcher("^\\s*(public|private|protected)\\s+(static|)\\s*(\\w+(?:<.+>|)(?:\\[\\]|))\\s+(\\w+)\\s*\\(\\s*.*\\s*\\)\\s*\\{", instruction);
         if(matcher.matches()) {
             MethodProtection protection = MethodProtection.PUBLIC;
-            if(matcher.group(1).equals("public")) {
-                protection = MethodProtection.PUBLIC;
-            } else if(matcher.group(1).equals("private")) {
-                protection = MethodProtection.PRIVATE;
-            } else if(matcher.group(1).equals("protected")) {
-                protection = MethodProtection.PROTECTED;
+            switch(matcher.group(1)) {
+                case "public":
+                    protection = MethodProtection.PUBLIC;
+                    break;
+                case "private":
+                    protection = MethodProtection.PRIVATE;
+                    break;
+                case "protected":
+                    protection = MethodProtection.PROTECTED;
             }
 
             MethodScope scope = MethodScope.DEFAULT;
